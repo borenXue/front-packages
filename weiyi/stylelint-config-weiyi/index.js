6f35d361751788bd6ff29feeeb7c7c48b9c94daf
borenXue <boren.xue@outlook.com>
2020-10-12 14:19:01 +0800

    feat(stylelint-config-weiyi): 修改规则 && 更新依赖版本

--- weiyi/stylelint-config-weiyi/index.js
+++ weiyi/stylelint-config-weiyi/index.js
@@ -14,5 +14,15 @@ module.exports = {
     'selector-max-specificity': '5,4,0',
     'selector-max-type': 5,
     'selector-no-qualifying-type': null,
+
+    'primer/no-override': null,
+    'selector-max-compound-selectors': 6,
+    'max-nesting-depth': 6,
+    'primer/no-unused-vars': null,
+    'declaration-property-value-blacklist': {
+      '/^background/': ['http:', 'https:'],
+      '/^border/': ['none'],
+      '/.+/': ['initial'],
+    },
   }
 }
