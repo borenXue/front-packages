521b5dc8d03d0dbea17e2e1ea879f4f3dde65763
hytor <hytor.yang@gmail.com>
2020-10-23 20:29:17 +0800

    fix(eslint-plugin-weiyi): 修复 weiyi config 规则表达中跳脱点号，去除 vue-recommended config 中冗余的 rules

--- weiyi/eslint-plugin-weiyi/src/configs/weiyi.js
+++ weiyi/eslint-plugin-weiyi/src/configs/weiyi.js
@@ -4,8 +4,8 @@ module.exports = {
     'weiyi/no-dead-protocol': 'error',
     'weiyi/no-third-party-res': [
       'error',
-      ['winbaoxian.(cn|com)'],
-      ['.(gif|png|jpg|jpeg|webp|js|css|json)$'],
+      ['winbaoxian\\.(cn|com)'],
+      ['\\.(gif|png|jpg|jpeg|webp|js|css|json)$'],
     ],
   },
 }
